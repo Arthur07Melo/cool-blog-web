b71f43247154e3bd45f9d1b36e1c6265dcdb0a90
Arthur07Melo <aarthur00melo@gmail.com>
2023-04-21 18:40:03 -0300

    updated about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,7 +5,13 @@ import Layout from "@/components/layout";
 export default function About() {
     return(
         <Layout>
-            This is the about page
+            <div className="flex flex-col items-center justify-center pt-10">
+                <p className="max-w-[75rem] text-lg">
+                This website was created with the purpose of serving a personal portfolio.
+                It should be a template for a forum page connected to an API that has users and posts, and some other features as like,
+                that is represented by a one to one connection in the database.
+                </p>
+            </div>
         </Layout>
     )
 }
