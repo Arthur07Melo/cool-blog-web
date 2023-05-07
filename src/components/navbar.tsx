d0dc5bb65d168ee20879aab5fc568b2f607733e2
Arthur07Melo <aarthur00melo@gmail.com>
2023-05-07 13:33:49 -0300

    adicionada responsividade na navbar, adicionado dialog de signup

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,19 +2,17 @@ import mainLogo from '../../public/mainLogo.png';
 import Image from "next/image"
 import Link from "next/link"
 import LoginDialog from './loginDialog';
+import SignupDialog from './signupDialog';
 
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between items-center p-5 text-lg h-[52px] bg-main-blue text-gray-300 px-10">
-            <div>
-                <Link className="text-xl" href="/"><Image src={mainLogo} height={52} alt='logo' /></Link>
-            </div>
-            <div className="flex gap-3">
+        <div className="flex flex-wrap justify-between items-center  text-lg  bg-main-blue text-gray-300 px-10">
+            <Link className="text-xl" href="/"><Image src={mainLogo} height={52} alt='logo' /></Link>
+            <div className="flex flex-wrap gap-3">
                 <LoginDialog />
-                <Link href="/signup">cadastrar</Link>
+                <SignupDialog />
                 <Link href="/categories">categorias</Link>
-                <Link href="/contact">contato</Link>
                 <Link href="/about">sobre</Link>
             </div>
         </div>
--- src/components/signupDialog.tsx
+++ src/components/signupDialog.tsx
@@ -1,18 +1,20 @@
 import  * as Dialog from '@radix-ui/react-dialog';
 
 
-export default function LoginDialog() {
+export default function SignupDialog() {
     return (
         <Dialog.Root>
             <Dialog.Trigger asChild>
-                <button>login</button>
+                <button>cadastrar</button>
             </Dialog.Trigger>
             <Dialog.Portal>
                 <Dialog.Overlay className='w-screen h-screen bg-black/70 fixed inset-0' />
                 <Dialog.Content className='fixed w-full max-w-sm rounded-md bg-slate-300 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 py-12'>
                     <div className='w-full h-full text-lg flex flex-col items-center justify-center'>
-                        <h1 className='text-3xl'>LOGIN</h1>
+                        <h1 className='text-3xl'>SIGNUP</h1>
                         <form action="" className='flex flex-col'>
+                            <label htmlFor="">username: </label>
+                            <input type="text" placeholder='escreva seu username...' className='border pl-2 border-gray-700 rounded-md' />
                             <label htmlFor="">email: </label>
                             <input type="text" placeholder='escreva seu email...' className='border pl-2 border-gray-700 rounded-md' />
                             <label htmlFor="">senha: </label>
